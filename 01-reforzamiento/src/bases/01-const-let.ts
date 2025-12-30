02d7fb16105afc5338c28fcc4043d515ad6470da
DevCabalceta <cabalceta.gabriel.2001@gmail.com>
2025-12-30 01:33:22 -0600

    primer commit

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,9 @@
+const firstname: string = 'Gabriel';
+let lastname = 'Cabalceta';
+
+let diceNumber  = 5;
+diceNumber = 3;
+
+const containsLetterC = lastname.includes('c');
+
+console.log({containsLetterC, diceNumber, firstname, lastname})
---
+++ 01-reforzamiento/src/bases/02-template-string.ts
@@ -0,0 +1,6 @@
+const firstname = 'Gabriel';
+const lastname = 'Cabalceta'
+
+const fullName = `${firstname} ${lastname}`
+
+console.log(fullName)
